2609b2972800ec35104e355141dabea12c899fcb
Henry Rothenberg <hsr205@aol.com>
2026-07-22 09:48:01 -0400

    Alter demo logic in main.py module

--- src/main/main.py
+++ src/main/main.py
@@ -2,14 +2,18 @@ import asyncio
 from logging import Logger
 
 from src.logger.logger import AppLogger
+from src.web_scraper.web_scraper import WebScraper
 
 
 async def main() -> int:
     logger: Logger = AppLogger().get_logger(class_name=str(__name__))
 
+    web_scraper:WebScraper = WebScraper()
+
     try:
 
-        logger.info(f"Hello from {__name__}")
+        await web_scraper.playwright_test_method()
+
         return 0
 
     except Exception as e:
