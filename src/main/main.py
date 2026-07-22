4368350728fb442d104d874542928d71ce87f40c
Henry Rothenberg <hsr205@aol.com>
2026-07-22 09:24:56 -0400

    Alter demo logic in main.py module

--- src/main/main.py
+++ src/main/main.py
@@ -1,13 +1,21 @@
-def main() -> int:
+import asyncio
+from logging import Logger
+
+from src.logger.logger import AppLogger
+
+
+async def main() -> int:
+    logger: Logger = AppLogger().get_logger(class_name=str(__name__))
+
     try:
 
-        print(f"Hello from {__name__}")
+        logger.info(f"Hello from {__name__}")
         return 0
 
     except Exception as e:
-        print(f"Exception thrown: {e}")
+        logger.exception(f"Exception thrown: {e}")
         raise Exception(e)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
