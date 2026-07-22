e90fb99f3e29dd18db7896a03fe6dd50ae6bb526
Henry Rothenberg <hsr205@aol.com>
2026-07-22 12:10:59 -0400

    Add insert method to database_client.py module

--- src/database/database_client.py
+++ src/database/database_client.py
@@ -18,7 +18,7 @@ class DatabaseClient:
         )
         self._logger: Logger = AppLogger.get_logger(self.__class__.__name__)
 
-    def create_franchises_table(self) -> None:
+    def create_franchise_table(self) -> None:
         self._logger.info(f"Creating connection to database: {self._settings.db_name}")
         with self._conn.cursor() as cursor:
             self._logger.info("Creating franchises table")
@@ -28,7 +28,38 @@ class DatabaseClient:
         self._logger.info(f"Closing connection to database: {self._settings.db_name}")
         self._logger.info("=" * 100)
 
-    def drop_franchises_table(self) -> None:
+    def insert_rows_into_franchises_table(self, nba_franchise_list: list[tuple]) -> None:
+        self._logger.info(f"Creating connection to database: {self._settings.db_name}")
+        with self._conn.cursor() as cursor:
+            self._logger.info(f"Inserting {len(nba_franchise_list)} rows in franchises table")
+
+            insert_query_str: str = """
+                      INSERT INTO franchise (
+                        franchise_name,
+                        league_name,
+                        year_established,
+                        current_year,
+                        num_years_in_operation,
+                        num_games_played,
+                        num_games_won,
+                        num_games_lost,
+                        win_percentage,
+                        playoff_appearances,
+                        division_title_wins,
+                        conference_title_wins,
+                        championship_title_wins
+                      )
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+            """
+
+            cursor.executemany(query=insert_query_str, vars_list=nba_franchise_list)
+
+            self._logger.info(f"Successfully inserted {len(nba_franchise_list)} rows in franchises table")
+            self._conn.commit()
+        self._logger.info(f"Closing connection to database: {self._settings.db_name}")
+        self._logger.info("=" * 100)
+
+    def drop_franchise_table(self) -> None:
         self._logger.info(f"Creating connection to database: {self._settings.db_name}")
         with self._conn.cursor() as cursor:
             self._logger.info("Dropping franchises table")
