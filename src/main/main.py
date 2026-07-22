fda9f942d4e399ca510f9e54a1b513202fc5e428
Henry Rothenberg <hsr205@aol.com>
2026-07-22 11:39:22 -0400

    WIP

--- src/main/main.py
+++ src/main/main.py
@@ -1,18 +1,22 @@
 import asyncio
 from logging import Logger
 
+from src.config.config import Settings
+from src.database.database_client import DatabaseClient
 from src.logger.logger import AppLogger
 from src.web_scraper.web_scraper import WebScraper
 
 
 async def main() -> int:
     logger: Logger = AppLogger().get_logger(class_name=str(__name__))
-
-    web_scraper:WebScraper = WebScraper()
+    settings: Settings = Settings()
+    web_scraper: WebScraper = WebScraper(settings=settings)
+    database_client: DatabaseClient = DatabaseClient(settings=settings)
 
     try:
 
-        await web_scraper.playwright_test_method()
+        await web_scraper.execute_web_scrapper()
+        # database_client.create_franchises_table()
 
         return 0
 
