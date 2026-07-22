1a71dee79ada95a355fba68d49c5825c42b9dc8a
Henry Rothenberg <hsr205@aol.com>
2026-07-22 11:38:43 -0400

    Add fields to config.py module

--- src/config/config.py
+++ src/config/config.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     base_url: str = Field(..., description="Initial URL to start web-scrapping from")
+    db_host: str = Field(..., description="PostgreSQL host")
+    db_port: int = Field(5432, description="PostgreSQL port")
+    db_name: str = Field(..., description="PostgreSQL database name")
+    db_user: str = Field(..., description="PostgreSQL user")
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
