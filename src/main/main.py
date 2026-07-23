0feb97f67ad68cf144b24b41be9c16e81adbc49d
Henry Rothenberg <hsr205@aol.com>
2026-07-22 21:41:47 -0400

    Alter execution logic in main.py

--- src/main/main.py
+++ src/main/main.py
@@ -4,21 +4,17 @@ from logging import Logger
 from src.config.config import Settings
 from src.database.database_client import DatabaseClient
 from src.logger.logger import AppLogger
-from src.web_scraper.web_scraper import WebScraper
 
 
 async def main() -> int:
     logger: Logger = AppLogger().get_logger(class_name=str(__name__))
     settings: Settings = Settings()
-    web_scraper: WebScraper = WebScraper(settings=settings)
     database_client: DatabaseClient = DatabaseClient(settings=settings)
 
     try:
 
-        nba_franchise_list:list[tuple] = await web_scraper.get_nba_franchise_list()
-        # database_client.create_franchise_table()
-        # database_client.drop_franchise_table()
-        database_client.insert_rows_into_franchises_table(nba_franchise_list=nba_franchise_list)
+        # database_client.create_player_table()
+        await database_client.insert_rows_into_player_table()
 
         return 0
 
