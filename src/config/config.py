c7f368722aec78a838e0e0a61de43f7a3574063c
Henry Rothenberg <hsr205@aol.com>
2026-07-24 11:51:28 -0400

    Add field to config.py module

--- src/config/config.py
+++ src/config/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     base_url: str = Field(..., description="Initial URL to start web-scrapping from")
+    stats_table_key: str = Field(...,
+                                 description="Unique ID that identifies which stats table to scrap from on a given player stats page")
     db_host: str = Field(..., description="PostgreSQL host")
     db_port: int = Field(5432, description="PostgreSQL port")
     db_name: str = Field(..., description="PostgreSQL database name")
