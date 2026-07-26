2163621a1ef51d7362567a0fb2790f4764232894
Henry Rothenberg <hsr205@aol.com>
2026-07-26 09:49:28 -0400

    Add load_data_into_franchise_table() method / alter load_data_into_table() method in data_loader.py module

--- src/database/data_loader.py
+++ src/database/data_loader.py
@@ -16,35 +16,78 @@ class DataLoader:
         self._database_client: DatabaseClient = DatabaseClient(settings=settings)
         self._logger: Logger = AppLogger.get_logger(self.__class__.__name__)
 
+    async def load_data_into_franchise_table(self) -> None:
+
+        current_franchise_dict: dict[int, str] = self._database_client.get_franchise_dict_from_franchise_table()
+
+        try:
+            async with async_playwright() as playwright_obj:
+                async with await playwright_obj.chromium.launch(headless=False) as browser:
+                    page = await browser.new_page()
+                    await self._web_scraper.navigate_to_base_url(page=page)
+
+                    for franchise_id, franchise_name in current_franchise_dict.items():
+                        try:
+                            stats_list: list[tuple] = await self._web_scraper.scrape_stats(
+                                page=page,
+                                search_name=franchise_name,
+                            )
+
+                            if not stats_list:
+                                self._logger.warning(f"No stats found for {franchise_name}, skipping")
+                                continue
+
+                            # Insert stats rows with the known player_id
+                            await self._database_client.insert_rows_into_franchise_per_season_stats_table(
+                                franchise_name_str=franchise_name,
+                                stats_list=stats_list,
+                            )
+
+                        except Exception as player_error:
+                            self._logger.warning(
+                                f"Failed to process '{franchise_name}', skipping — reason: {player_error}")
+                            await self._web_scraper.navigate_to_base_url(page=page)
+                            continue
+        finally:
+            self._logger.info("Closing connection pool")
+            self._logger.info("=" * 100)
+            self._database_client.close_pool()
+
     async def load_data_into_table(self, insert_query_str: str) -> None:
 
         player_dict: dict[int, str] = self._database_client.get_player_dict_from_player_table()
 
-        async with async_playwright() as playwright_obj:
-            async with await playwright_obj.chromium.launch(headless=False) as browser:
-                page = await browser.new_page()
-                await self._web_scraper.navigate_to_base_url(page=page)
+        try:
+            async with async_playwright() as playwright_obj:
+                async with await playwright_obj.chromium.launch(headless=False) as browser:
+                    page = await browser.new_page()
+                    await self._web_scraper.navigate_to_base_url(page=page)
 
-                for player_id, player_name in player_dict.items():
-                    try:
-                        stats_list: list[tuple] = await self._web_scraper.scrape_player_stats(
-                            page=page,
-                            player_name=player_name,
-                        )
+                    for player_id, player_name in player_dict.items():
+                        try:
+                            stats_list: list[tuple] = await self._web_scraper.scrape_stats(
+                                page=page,
+                                search_name=player_name,
+                            )
 
-                        if not stats_list:
-                            self._logger.warning(f"No stats found for {player_name}, skipping")
+                            if not stats_list:
+                                self._logger.warning(f"No stats found for {player_name}, skipping")
+                                continue
+
+                            # Insert stats rows with the known player_id
+                            await self._database_client.insert_rows_into_table(
+                                player_name_str=player_name,
+                                player_regular_season_stats_list=stats_list,
+                                current_player_dict=player_dict,
+                                insert_query_str=insert_query_str
+                            )
+
+                        except Exception as player_error:
+                            self._logger.warning(
+                                f"Failed to process '{player_name}', skipping — reason: {player_error}")
+                            await self._web_scraper.navigate_to_base_url(page=page)
                             continue
-
-                        # Insert stats rows with the known player_id
-                        await self._database_client.insert_rows_into_table(
-                            player_name_str=player_name,
-                            player_regular_season_stats_list=stats_list,
-                            current_player_dict=player_dict,
-                            insert_query_str=insert_query_str
-                        )
-
-                    except Exception as player_error:
-                        self._logger.warning(f"Failed to process '{player_name}', skipping — reason: {player_error}")
-                        await self._web_scraper.navigate_to_base_url(page=page)
-                        continue
+        finally:
+            self._logger.info("Closing connection pool")
+            self._logger.info("=" * 100)
+            self._database_client.close_pool()
