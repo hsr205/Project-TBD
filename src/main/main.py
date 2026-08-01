294e98b921e4e84d32de87547de2ae9e04a1616c
Henry Rothenberg <hsr205@aol.com>
2026-08-01 13:07:14 -0400

    Alter demo logic in main.py module

--- src/main/main.py
+++ src/main/main.py
@@ -1,11 +1,11 @@
 import asyncio
 from logging import Logger
 
+from models.umap_rendering import UMAPRendering
 from src.config.config import Settings
 from src.database.data_loader import DataLoader
 from src.database.database_client import DatabaseClient
 from src.logger.logger import AppLogger
-from src.utils.constants import Constants
 
 
 async def main() -> int:
@@ -18,13 +18,12 @@ async def main() -> int:
 
     try:
 
-        player_dict: dict[int, str] = database_client.get_player_dict_from_player_table()
-
         logger.info("Executing application")
         logger.info("=" * 100)
-        await data_loader.load_data_into_table(
-            insert_query_str=Constants.Queries.INSERT_INTO_PLAYER_PLAYOFF_SERIES_STATS_TABLE_STR,
-            entity_dict=player_dict)
+
+        umap_rending: UMAPRendering = UMAPRendering(settings=settings)
+
+        umap_rending.display_umap_rendering_for_advanced_stats()
 
         logger.info("Successfully completed application execution")
         logger.info("=" * 100)
