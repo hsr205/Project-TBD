bb5ba3286f17098835adb87f7545b5ef1a72e0e4
Henry Rothenberg <hsr205@aol.com>
2026-08-07 12:50:55 -0400

    Remove unused demo logic from main.py module

--- src/main/main.py
+++ src/main/main.py
@@ -3,8 +3,6 @@ from logging import Logger
 
 from models.umap_rendering import UMAPRendering
 from src.config.config import Settings
-from src.database.data_loader import DataLoader
-from src.database.database_client import DatabaseClient
 from src.logger.logger import AppLogger
 
 
@@ -12,9 +10,6 @@ async def main() -> int:
     logger: Logger = AppLogger().get_logger(class_name=str(__name__))
     settings: Settings = Settings()
 
-    data_loader: DataLoader = DataLoader(settings)
-
-    database_client: DatabaseClient = DatabaseClient(settings=settings)
 
     try:
 
