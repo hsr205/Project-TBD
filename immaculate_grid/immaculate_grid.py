b53238d6eb157384456d44055231ef47485c4ca6
Henry Rothenberg <hsr205@aol.com>
2026-08-10 15:41:55 -0400

    WIP

--- immaculate_grid/immaculate_grid.py
+++ immaculate_grid/immaculate_grid.py
@@ -27,59 +27,47 @@ class ImmaculateGrid:
             element_one_str: str = tuple_obj[0]
             element_two_str: str = tuple_obj[1]
 
-            # self._logger.info(f"({element_one_str}, {element_two_str})")
+            self._logger.info(f"({element_one_str}, {element_two_str})")
 
             initial_category_str: str = Constants.TEAM_ABBREVIATION_DICT.get(element_one_str, '')
             secondary_category_str: str = Constants.TEAM_ABBREVIATION_DICT.get(element_two_str, '')
 
-            if initial_category_str == '':
-                self._logger.info(f"({element_one_str}, {element_two_str})")
+            sql_condition_str_1: str = self._get_sql_condition_str(category_str=initial_category_str,
+                                                                   element_str=element_one_str)
 
-                split_list:list[str] = element_one_str.split()
-                test_list:list = [x.replace('+', '') for x in split_list]
+            sql_condition_str_2: str = self._get_sql_condition_str(category_str=secondary_category_str,
+                                                                   element_str=element_two_str)
 
-                quantity_value: float = 0.0
+            query_elements_list: list[str] = []
 
-                for element in test_list:
+            if sql_condition_str_1 == '' and sql_condition_str_2 == '':
+                query_elements_list.append(initial_category_str)
+                query_elements_list.append(secondary_category_str)
 
-                    if element.isdigit():
-                        quantity_value = float(element)
+            else:
 
-                    if quantity_value > 0.0:
-                        self._logger.info(f"result = {Constants.ImmaculateGridCategories.IMMACULATE_GRID_CATEGORY_SQL_MAPPING_DICT.get(element, '')}{quantity_value}")
+                if sql_condition_str_1 == '':
+                    query_elements_list.append(sql_condition_str_2)
+                    query_elements_list.append(initial_category_str)
 
-                self._logger.info("=" * 100)
+                if sql_condition_str_2 == '':
+                    query_elements_list.append(sql_condition_str_1)
+                    query_elements_list.append(secondary_category_str)
 
-            if secondary_category_str == '':
-                self._logger.info("secondary_category_str == ''")
-                self._logger.info(f"({element_one_str}, {element_two_str})")
+                if sql_condition_str_1 != '' and sql_condition_str_2 != '':
+                    query_elements_list.append(sql_condition_str_1)
+                    query_elements_list.append(sql_condition_str_2)
 
-                split_list:list[str] = element_two_str.split()
-                test_list:list = [x.replace('+', '') for x in split_list]
-
-                quantity_value: float = 0.0
-
-                for element in test_list:
-
-                    if element.isdigit():
-                        quantity_value = float(element)
-
-                for element in test_list:
-
-                    if element.isdigit():
-                        quantity_value = float(element)
-
-                    if quantity_value > 0.0:
-                        self._logger.info(
-                            f"result = {Constants.ImmaculateGridCategories.IMMACULATE_GRID_CATEGORY_SQL_MAPPING_DICT.get(element, '')}{quantity_value}")
-
-                self._logger.info("=" * 100)
+            self._logger.info(f"query_elements_list = {query_elements_list}")
+            self._logger.info("=" * 100)
 
             if initial_category_str == 'N/A' or secondary_category_str == 'N/A':
                 continue
 
-            # team_abbreviation_tuple: tuple = tuple([team_abbreviation_one_text, team_abbreviation_two_text])
-            #
+            # if sql_condition_str_1 == '':
+            #     team_abbreviation_list[0] = sql_condition_str_1
+            #     team_abbreviation_list[1] =
+
             # column_names_list, query_result_list = self._database_client.get_immaculate_grid_query_results(
             #     team_abbreviation_tuple=team_abbreviation_tuple)
             #
@@ -91,3 +79,27 @@ class ImmaculateGrid:
         result_matrix: np.ndarray = np.array(player_result_list).reshape(3, 3)
 
         print(result_matrix)
+
+    def _get_sql_condition_str(self, category_str: str, element_str: str) -> str:
+
+        result_sql_condition_str: str = ""
+
+        if category_str == '':
+
+            split_list: list[str] = element_str.split()
+            test_list: list = [x.replace('+', '') for x in split_list]
+
+            quantity_value: float = 0.0
+
+            for element in test_list:
+
+                if element.isdigit():
+                    quantity_value = float(element)
+
+                sql_condition_str: str = Constants.ImmaculateGridCategories.IMMACULATE_GRID_CATEGORY_SQL_MAPPING_DICT.get(
+                    element, '')
+
+                if quantity_value > 0.0 and sql_condition_str != '':
+                    result_sql_condition_str += sql_condition_str + ' ' + str(quantity_value)
+
+        return result_sql_condition_str
