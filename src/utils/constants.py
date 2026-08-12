3fa2282b34db630947b45f32aee261f1e14da081
Henry Rothenberg <hsr205@aol.com>
2026-08-12 19:17:59 -0400

    Add US_STATES_LIST field to constants.py module

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -14,6 +14,19 @@ class Constants:
         "franchise-roBWT3o": ["table#", 17]
     }
 
+    US_STATES_LIST: list[str] = [
+        "Alabama", "Alaska", "Arizona", "Arkansas", "California",
+        "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+        "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+        "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+        "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+        "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+        "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+        "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+        "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+        "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+    ]
+
     TEAM_ABBREVIATION_DICT: dict[str, str] = {
         "Atlanta Hawks": "ATL",
         "Boston Celtics": "BOS",
